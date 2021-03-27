9aa04f3ebe447a84d09414ca3ac0c00466add344
Richard Gooch <rg+github0@safe-mbox.com>
2021-03-27 09:00:18 -0700

    Add Imaginator.GetDirectedGraph SRPC method.

---
+++ imagebuilder/rpcd/getDirectedGraph.go
@@ -0,0 +1,16 @@
+package rpcd
+
+import (
+	"github.com/Cloud-Foundations/Dominator/lib/errors"
+	"github.com/Cloud-Foundations/Dominator/lib/srpc"
+	proto "github.com/Cloud-Foundations/Dominator/proto/imaginator"
+)
+
+func (t *srpcType) GetDirectedGraph(conn *srpc.Conn,
+	request proto.GetDirectedGraphRequest,
+	reply *proto.GetDirectedGraphResponse) error {
+	data, err := t.builder.GetDirectedGraph()
+	reply.GraphvizDot = data
+	reply.Error = errors.ErrorToString(err)
+	return nil
+}
