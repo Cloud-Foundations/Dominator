16484dd664fb242bb21f189155424fde48aa62ab
Richard Gooch <rg+github0@safe-mbox.com>
2022-02-09 06:18:51 -0800

    dominator: add support for listing images for subs in CSV format.

--- dom/herd/showImagesForSubs.go
+++ dom/herd/showImagesForSubs.go
@@ -2,6 +2,7 @@ package herd
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 	"github.com/Cloud-Foundations/Dominator/lib/html"
 	"github.com/Cloud-Foundations/Dominator/lib/json"
 	"github.com/Cloud-Foundations/Dominator/lib/srpc"
+	"github.com/Cloud-Foundations/Dominator/lib/url"
 )
 
 type imageSubType struct {
@@ -26,7 +28,18 @@ func (herd *Herd) listImagesForSubsHandler(w http.ResponseWriter,
 	req *http.Request) {
 	writer := bufio.NewWriter(w)
 	defer writer.Flush()
-	herd.showImagesForSubsJSON(writer)
+	parsedQuery := url.ParseQuery(req.URL)
+	switch parsedQuery.OutputType() {
+	case url.OutputTypeCsv:
+		herd.showImagesForSubsCSV(writer)
+	case url.OutputTypeHtml: // Want the benchmarking endpoint instead.
+		fmt.Fprintln(writer, "HTML output not supported")
+	case url.OutputTypeJson:
+		herd.showImagesForSubsJSON(writer)
+	case url.OutputTypeText:
+		fmt.Fprintln(writer, "Text output not supported")
+	}
+	fmt.Fprintln(writer, "Unknown output type")
 }
 
 func (herd *Herd) showImagesForSubsHandler(w io.Writer, req *http.Request) {
@@ -60,6 +73,30 @@ func (herd *Herd) showImagesForSubsHTML(writer io.Writer) {
 		showImagesForSub(tw, sub)
 	}
 	fmt.Fprintln(writer, "</table>")
+}
+
+func (herd *Herd) showImagesForSubsCSV(writer io.Writer) {
+	subs := herd.getSelectedSubs(selectAliveSub)
+	w := csv.NewWriter(writer)
+	defer w.Flush()
+	w.Write([]string{
+		"Hostname",
+		"Required Image",
+		"Planned Image",
+		"Status",
+		"Last Image Update",
+		"Last Note",
+	})
+	for _, sub := range subs {
+		w.Write([]string{
+			sub.mdb.Hostname,
+			sub.mdb.RequiredImage,
+			sub.mdb.PlannedImage,
+			sub.publishedStatus.String(),
+			sub.lastSuccessfulImageName,
+			sub.lastNote,
+		})
+	}
 }
 
 func (herd *Herd) showImagesForSubsJSON(writer io.Writer) {
