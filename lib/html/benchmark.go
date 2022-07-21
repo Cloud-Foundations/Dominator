0be59f1af0263d4c49a6d76d827a195432b2477f
Richard Gooch <rg+github0@safe-mbox.com>
2022-07-21 08:38:02 -0700

    lib/html: unlock OS thread to ensure threads are re-used.

--- lib/html/benchmark.go
+++ lib/html/benchmark.go
@@ -15,6 +15,7 @@ func benchmarkedHandler(handler func(io.Writer,
 	*http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		runtime.LockOSThread()
+		defer runtime.UnlockOSThread()
 		var startRusage, stopRusage wsyscall.Rusage
 		wsyscall.Getrusage(wsyscall.RUSAGE_THREAD, &startRusage)
 		startTime := time.Now()
