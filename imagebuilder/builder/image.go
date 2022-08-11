b3c9a1123060bc6aac67e3830b5060e58b0d35a7
Richard Gooch <rg+github0@safe-mbox.com>
2022-08-11 06:58:29 -0700

    imagebuilder/builder: make use of lib/fsutil.GetTreeSize() function.

--- imagebuilder/builder/image.go
+++ imagebuilder/builder/image.go
@@ -214,22 +214,6 @@ func (stream *imageStreamType) getSourceImage(b *Builder, buildLog io.Writer) (
 		&manifest, nil
 }
 
-func getTreeSize(dirname string) (uint64, error) {
-	var size uint64
-	err := filepath.Walk(dirname,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			size += uint64(info.Size())
-			return nil
-		})
-	if err != nil {
-		return 0, err
-	}
-	return size, nil
-}
-
 func listDirectory(directoryName string) ([]string, error) {
 	directory, err := os.Open(directoryName)
 	if err != nil {
