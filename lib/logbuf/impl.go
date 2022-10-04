0cb7368e054585d8d9547496d14011da32f4b34f
Camilo Viecco <cviecco@purestorage.com>
2022-10-04 09:13:44 -0700

    cleaner files

--- lib/logbuf/impl.go
+++ lib/logbuf/impl.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/Cloud-Foundations/Dominator/lib/bufwriter"
 	"github.com/Cloud-Foundations/Dominator/lib/flagutil"
 	"github.com/Cloud-Foundations/Dominator/lib/format"
 )
@@ -179,6 +180,28 @@ func (lb *LogBuffer) closeAndOpenNewFile() error {
 		return err
 	}
 	return nil
+}
+
+// This should be called with the lock held.
+func (lb *LogBuffer) openNewFile() error {
+	lb.fileSize = 0
+	filename := time.Now().Format(timeLayout)
+	file, err := os.OpenFile(path.Join(lb.options.Directory, filename),
+		os.O_CREATE|os.O_WRONLY, filePerms)
+	if err != nil {
+		return err
+	}
+	if lb.options.RedirectStderr {
+		localDup(int(file.Fd()), int(os.Stdout.Fd()))
+		localDup(int(file.Fd()), int(os.Stderr.Fd()))
+	}
+	lb.file = file
+	lb.writer = bufwriter.NewWriter(file, time.Second)
+	symlink := path.Join(lb.options.Directory, "latest")
+	tmpSymlink := symlink + "~"
+	os.Remove(tmpSymlink)
+	os.Symlink(filename, tmpSymlink)
+	return os.Rename(tmpSymlink, symlink)
 }
 
 // This should be called with the lock held.
