ff30bbae449c4e527f0e4e499eff07fda0bcd80d
Richard Gooch <rg+github0@safe-mbox.com>
2023-02-04 09:50:47 -0800

    lib/fsutil: add locking around losetup operations to fix race condition.

--- lib/fsutil/loopback.go
+++ lib/fsutil/loopback.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
+var losetupMutex sync.Mutex
+
 func loopbackDelete(loopDevice string) error {
+	losetupMutex.Lock()
+	defer losetupMutex.Unlock()
 	return exec.Command("losetup", "-d", loopDevice).Run()
 }
 
 func loopbackSetup(filename string) (string, error) {
+	losetupMutex.Lock()
+	defer losetupMutex.Unlock()
 	cmd := exec.Command("losetup", "-fP", "--show", filename)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
