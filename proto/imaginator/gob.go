94ae7f6c744c2e879e41970f4bd1b0a4237ea9a0
Richard Gooch <rg+github0@safe-mbox.com>
2023-02-10 06:22:50 -0800

    proto/imaginator: register types with GOB to avoid spurious decode failures.

---
+++ proto/imaginator/gob.go
@@ -0,0 +1,15 @@
+package imaginator
+
+import (
+	"encoding/gob"
+
+	"github.com/Cloud-Foundations/Dominator/lib/filesystem"
+)
+
+func init() {
+	gob.Register(&filesystem.RegularInode{})
+	gob.Register(&filesystem.ComputedRegularInode{})
+	gob.Register(&filesystem.SymlinkInode{})
+	gob.Register(&filesystem.SpecialInode{})
+	gob.Register(&filesystem.DirectoryInode{})
+}
