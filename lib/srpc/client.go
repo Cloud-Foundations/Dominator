Cloud-Foundations/Dominator

commit a3ff8c6940cfaa0006b29436f0b1b8c11533aa26

lib/srpc: add Client.IsClosed() method.

lib/srpc/client.go
@@ -324,12 +324,12 @@ func (client *Client) close() error {
 		clientMetricsMutex.Lock()
 		numOpenClientConnections--
 		clientMetricsMutex.Unlock()
-		err := client.conn.Close()
+		conn := client.conn
 		client.conn = nil
-		return err
+		return conn.Close()
 	}
-	client.resource.resource.Release()
 	client.conn = nil
+	client.resource.resource.Release()
 	clientMetricsMutex.Lock()
 	if client.resource.inUse {
 		numInUseClientConnections--

lib/srpc/api.go
@@ -381,6 +381,11 @@ func (client *Client) Call(serviceMethod string) (*Conn, error) {
 	return client.call(serviceMethod)
 }
 
+// IsClosed will return true of the client is closed.
+func (client *Client) IsClosed() bool {
+	return client.conn == nil
+}
+
 // IsEncrypted will return true if the underlying connection is TLS-encrypted.
 func (client *Client) IsEncrypted() bool {
 	return client.isEncrypted

commit 0b2cdeb56a08e1b74c56812caf33b18c0ba583e8

lib/srpc: fix panic introduced in a3ff8c6940cfaa0006b29436f0b1b8c11533aa26.

lib/srpc/client.go
@@ -332,8 +332,8 @@ func (client *Client) close() error {
 		client.conn = nil
 		return conn.Close()
 	}
-	client.conn = nil
 	client.resource.resource.Release()
+	client.conn = nil
 	clientMetricsMutex.Lock()
 	if client.resource.inUse {
 		numInUseClientConnections--