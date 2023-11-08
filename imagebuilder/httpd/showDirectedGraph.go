ec47947edbe51b697a272b1f9b167b5f7ff76da3
Richard Gooch <rg+github0@safe-mbox.com>
2023-11-08 08:29:04 -0800

    imagebuilder/httpd: show failed fetch logs when showing directed graph.

--- imagebuilder/httpd/showDirectedGraph.go
+++ imagebuilder/httpd/showDirectedGraph.go
@@ -53,48 +53,50 @@ func (s state) writeDirectedGraph(writer io.Writer,
 	}
 	if result.GeneratedAt.IsZero() { // No data yet.
 		fmt.Fprintln(writer, "No data generated yet<br>")
-		if time.Since(processStartTime) > 2*time.Second {
-			fmt.Fprintln(writer,
-				`<form enctype="application/x-www-form-urlencoded" action="/showDirectedGraph" method="post">`)
-			fmt.Fprintln(writer,
-				`<input type="submit" value="Regenerate">`)
-		}
-		return
-	}
-	cmd := exec.Command("dot", "-Tsvg")
-	cmd.Stdin = bytes.NewReader(result.GraphvizDot)
-	cmd.Stdout = writer
-	cmd.Stderr = writer
-	err = cmd.Run()
-	if err == nil {
-		fmt.Fprintln(writer, "<p>")
 	} else {
-		fmt.Fprintf(writer, "error rendering graph: %s<br>\n", err)
-		fmt.Fprintln(writer, "Showing graph data:<br>")
-		fmt.Fprintln(writer, "<pre>")
-		writer.Write(result.GraphvizDot)
-		fmt.Fprintln(writer, "</pre>")
-	}
-	weblinks, _ := s.builder.GetRelationshipsQuickLinks()
-	for _, weblink := range weblinks {
-		fmt.Fprintf(writer,
-			"<a href=\"/showDirectedGraph%s\">%s</a>&nbsp;&nbsp;&nbsp;\n",
-			weblink.URL, weblink.Name)
-	}
-	if len(weblinks) > 0 {
-		fmt.Fprintln(writer, "<br>")
+		cmd := exec.Command("dot", "-Tsvg")
+		cmd.Stdin = bytes.NewReader(result.GraphvizDot)
+		cmd.Stdout = writer
+		cmd.Stderr = writer
+		err = cmd.Run()
+		if err == nil {
+			fmt.Fprintln(writer, "<p>")
+		} else {
+			fmt.Fprintf(writer, "error rendering graph: %s<br>\n", err)
+			fmt.Fprintln(writer, "Showing graph data:<br>")
+			fmt.Fprintln(writer, "<pre>")
+			writer.Write(result.GraphvizDot)
+			fmt.Fprintln(writer, "</pre>")
+		}
+		weblinks, _ := s.builder.GetRelationshipsQuickLinks()
+		for _, weblink := range weblinks {
+			fmt.Fprintf(writer,
+				"<a href=\"/showDirectedGraph%s\">%s</a>&nbsp;&nbsp;&nbsp;\n",
+				weblink.URL, weblink.Name)
+		}
+		if len(weblinks) > 0 {
+			fmt.Fprintln(writer, "<br>")
+		}
 	}
 	if len(result.FetchLog) > 0 {
-		fmt.Fprintln(writer,
-			"<hr style=\"height:2px\"><font color=\"#bbb\">")
-		fmt.Fprintln(writer, "<b>Fetch log:</b>")
+		if result.LastAttemptError == "" {
+			fmt.Fprintln(writer,
+				"<hr style=\"height:2px\"><font color=\"#bbb\">")
+			fmt.Fprintln(writer, "<b>Fetch log:</b>")
+		} else {
+			fmt.Fprintln(writer,
+				"<hr style=\"height:2px\"><font color=\"#fbb\">")
+			fmt.Fprintln(writer, "<b>Failed fetch log:</b>")
+		}
 		fmt.Fprintln(writer, "<pre>")
 		writer.Write(result.FetchLog)
 		fmt.Fprintln(writer, "</pre>")
 		fmt.Fprintln(writer, "</font>")
 	}
-	fmt.Fprintf(writer, "Data generated at: %s<br>\n",
-		result.GeneratedAt.Format(format.TimeFormatSeconds))
+	if !result.GeneratedAt.IsZero() {
+		fmt.Fprintf(writer, "Data generated at: %s<br>\n",
+			result.GeneratedAt.Format(format.TimeFormatSeconds))
+	}
 	if result.LastAttemptError != "" {
 		fmt.Fprintf(writer,
 			"Last generation attempt at: %s failed: %s<br>\n",
