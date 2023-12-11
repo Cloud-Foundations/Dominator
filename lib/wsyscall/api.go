18bb4c14f2b2baa9755d141d16fb834ff8b934f0
Richard Gooch <rg+github0@safe-mbox.com>
2023-12-11 07:17:46 -0800

    lib/wsyscall: add FALLOC_FL_* constants.

--- lib/wsyscall/api.go
+++ lib/wsyscall/api.go
@@ -3,6 +3,14 @@ package wsyscall
 import "syscall"
 
 const (
+	FALLOC_FL_COLLAPSE_RANGE = 0x8
+	FALLOC_FL_INSERT_RANGE   = 0x20
+	FALLOC_FL_KEEP_SIZE      = 0x1
+	FALLOC_FL_NO_HIDE_STALE  = 0x4
+	FALLOC_FL_PUNCH_HOLE     = 0x2
+	FALLOC_FL_UNSHARE_RANGE  = 0x40
+	FALLOC_FL_ZERO_RANGE     = 0x10
+
 	MS_BIND = 1 << iota
 	MS_RDONLY
 
