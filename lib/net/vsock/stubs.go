035d07a17a5f233b7602884125b681756cad656e
Richard Gooch <rg+github0@safe-mbox.com>
2024-04-27 09:05:57 -0700

    lib/net/vsock: add CheckVsockets() function.

--- lib/net/vsock/stubs.go
+++ lib/net/vsock/stubs.go
@@ -1,8 +1,12 @@
-// +build !linux
+//go:build !linux
 
 package vsock
 
 import "syscall"
+
+func checkVsockets() error {
+	return syscall.ENOTSUP
+}
 
 func getContextID() (uint32, error) {
 	return 0, syscall.ENOTSUP
