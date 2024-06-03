cf9211d9e9055c8a1a56ebeb70e1ee16f2ba4d3e
Richard Gooch <rg+github0@safe-mbox.com>
2024-06-03 06:47:11 -0700

    hypervisor/httpd: show volume directories in their preferred order of use.

--- hypervisor/httpd/showVolumeDirectories.go
+++ hypervisor/httpd/showVolumeDirectories.go
@@ -4,13 +4,30 @@ import (
 	"bufio"
 	"net/http"
 
+	"github.com/Cloud-Foundations/Dominator/hypervisor/manager"
 	"github.com/Cloud-Foundations/Dominator/lib/json"
 )
 
+type volumeEntry struct {
+	Directory string
+	manager.VolumeInfo
+}
+
 func (s state) showVolumeDirectoriesHandler(w http.ResponseWriter,
 	req *http.Request) {
+	volumeDirectories := s.manager.ListVolumeDirectories()
+	volumeInfos := s.manager.GetVolumeDirectories()
+	volumeEntries := make([]volumeEntry, 0, len(volumeDirectories))
+	for _, directory := range volumeDirectories {
+		if _, ok := volumeInfos[directory]; !ok {
+			continue
+		}
+		volumeEntries = append(volumeEntries, volumeEntry{
+			Directory:  directory,
+			VolumeInfo: volumeInfos[directory],
+		})
+	}
 	writer := bufio.NewWriter(w)
 	defer writer.Flush()
-	volumeDirectories := s.manager.GetVolumeDirectories()
-	json.WriteWithIndent(writer, "    ", volumeDirectories)
+	json.WriteWithIndent(writer, "    ", volumeEntries)
 }
