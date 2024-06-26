fa63aeed8f9ca0b4a7d963c01326cb09fcf05637
Richard Gooch <rg+github0@safe-mbox.com>
2024-06-25 22:08:17 -0700

    cmd/vm-control: add change-vm-cpu-priority subcommand and -cpuPriority flag.

---
+++ cmd/vm-control/changeVmCpuPriority.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/Cloud-Foundations/Dominator/lib/errors"
+	"github.com/Cloud-Foundations/Dominator/lib/log"
+	proto "github.com/Cloud-Foundations/Dominator/proto/hypervisor"
+)
+
+func changeVmCpuPrioritySubcommand(args []string,
+	logger log.DebugLogger) error {
+	if err := changeVmCpuPriority(args[0], logger); err != nil {
+		return fmt.Errorf("error changing VM CPU priority: %s", err)
+	}
+	return nil
+}
+
+func changeVmCpuPriority(vmHostname string,
+	logger log.DebugLogger) error {
+	if vmIP, hypervisor, err := lookupVmAndHypervisor(vmHostname); err != nil {
+		return err
+	} else {
+		return changeVmCpuPriorityOnHypervisor(hypervisor, vmIP, logger)
+	}
+}
+
+func changeVmCpuPriorityOnHypervisor(hypervisor string, ipAddr net.IP,
+	logger log.DebugLogger) error {
+	request := proto.ChangeVmCpuPriorityRequest{
+		CpuPriority: *cpuPriority,
+		IpAddress:   ipAddr,
+	}
+	client, err := dialHypervisor(hypervisor)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	var reply proto.ChangeVmOwnerUsersResponse
+	err = client.RequestReply("Hypervisor.ChangeVmCpuPriority",
+		request, &reply)
+	if err != nil {
+		return err
+	}
+	return errors.New(reply.Error)
+}
