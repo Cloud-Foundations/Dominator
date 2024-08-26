9aea6a253b44ae94a83031cba10a8bd158fab9d5
Richard Gooch <rg+github0@safe-mbox.com>
2024-08-26 06:05:50 -0700

    lib/backoffdelay: add more Exponential methods for interval management.

--- lib/backoffdelay/api.go
+++ lib/backoffdelay/api.go
@@ -10,6 +10,7 @@ type Exponential struct {
 	maximum    time.Duration
 	minimum    time.Duration
 	sleepFunc  func(time.Duration)
+	stopTime   time.Time
 }
 
 type Resetter interface {
@@ -24,7 +25,7 @@ type Sleeper interface {
 // If minimumDelay is less than or equal to 0, the default is 1 second.
 // If maximumDelay is less than or equal to minimumDelay, the default is 10
 // times minimumDelay.
-// The Sleep interval will increase by a factor of 2 raised to the power of
+// The Sleep duration will increase by a factor of 2 raised to the power of
 // -growthRate. For example:
 // 0: 1x
 // 1: 0.5x
@@ -34,10 +35,31 @@ func NewExponential(minimumDelay, maximumDelay time.Duration,
 	return newExponential(minimumDelay, maximumDelay, growthRate)
 }
 
+// RemainingInterval will return the time remaining until the end of the
+// interval that was started when StartInterval was called.
+func (e *Exponential) RemainingInterval() time.Duration {
+	return e.remainingInterval()
+}
+
+// Reset will set the sleep duration to the minimum delay.
 func (e *Exponential) Reset() {
 	e.reset()
 }
 
+// Sleep will sleep and then increase the duration for the next Sleep, until
+// reaching the maximum delay.
 func (e *Exponential) Sleep() {
 	e.sleep()
+}
+
+// SleepUntilEnd will sleep until the end of the interval that was started when
+// StartInterval was called.
+func (e *Exponential) SleepUntilEnd() {
+	e.sleepUntilEnd()
+}
+
+// StartInterval starts an interval. This may be used to impose a maximum loop
+// duration when iterating over a set of operations with individual timeouts.
+func (e *Exponential) StartInterval() {
+	e.startInterval()
 }
