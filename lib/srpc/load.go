183295b43c1af73cde08df0cd36c7b34231ae2be
Richard Gooch <rg+github0@safe-mbox.com>
2024-09-11 21:43:11 -0700

    lib/srpc: also load {cert,key}.pem in LoadCertificates() function.

--- lib/srpc/load.go
+++ lib/srpc/load.go
@@ -5,12 +5,13 @@ import (
 	"crypto/x509"
 	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 	"sort"
 	"strings"
 	"time"
 
 	"github.com/Cloud-Foundations/Dominator/lib/format"
+	"github.com/Cloud-Foundations/Dominator/lib/stringutil"
 	"github.com/Cloud-Foundations/Dominator/lib/x509util"
 )
 
@@ -20,20 +21,28 @@ func loadCertificates(directory string) ([]tls.Certificate, error) {
 		return nil, err
 	}
 	names, err := dir.Readdirnames(0)
-	defer dir.Close()
 	if err != nil {
 		return nil, err
 	}
+	dir.Close()
 	certs := make([]tls.Certificate, 0, len(names)/2)
 	now := time.Now()
 	for _, keyName := range names {
-		if !strings.HasSuffix(keyName, ".key") {
+		var certName string
+		if keyName == "key.pem" {
+			_, ok := stringutil.ConvertListToMap(names, false)["cert.pem"]
+			if !ok {
+				continue
+			}
+			certName = "cert.pem"
+		} else if !strings.HasSuffix(keyName, ".key") {
 			continue
+		} else {
+			certName = keyName[:len(keyName)-3] + "cert"
 		}
-		certName := keyName[:len(keyName)-3] + "cert"
 		cert, err := tls.LoadX509KeyPair(
-			path.Join(directory, certName),
-			path.Join(directory, keyName))
+			filepath.Join(directory, certName),
+			filepath.Join(directory, keyName))
 		if err != nil {
 			return nil, fmt.Errorf("unable to load keypair: %s", err)
 		}
