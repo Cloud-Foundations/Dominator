4795317bef6b3241614d8fbeb373aaa84899f5ae
Richard Gooch <rg+github0@safe-mbox.com>
2024-09-14 09:44:15 -0700

    hypervisor/manager: validate identity before taking VM lock.

--- hypervisor/manager/vmCredentials.go
+++ hypervisor/manager/vmCredentials.go
@@ -10,16 +10,16 @@ import (
 func (m *Manager) replaceVmCredentials(
 	request proto.ReplaceVmCredentialsRequest,
 	authInfo *srpc.AuthInformation) error {
-	vm, err := m.getVmLockAndAuth(request.IpAddress, true, authInfo, nil)
-	if err != nil {
-		return err
-	}
-	defer vm.mutex.Unlock()
 	identityName, identityExpires, err := validateIdentityKeyPair(
 		request.IdentityCertificate, request.IdentityKey, authInfo.Username)
 	if err != nil {
 		return err
 	}
+	vm, err := m.getVmLockAndAuth(request.IpAddress, true, authInfo, nil)
+	if err != nil {
+		return err
+	}
+	defer vm.mutex.Unlock()
 	err = writeKeyPair(request.IdentityCertificate, request.IdentityKey,
 		filepath.Join(vm.dirname, IdentityCertFile),
 		filepath.Join(vm.dirname, IdentityKeyFile))
