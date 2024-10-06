18ed88a785c3b71e3f5b7d64db44ce7d9a5dc150
Richard Gooch <rg+github0@safe-mbox.com>
2024-10-06 07:26:39 -0700

    cmd/vm-control: add -machineType option & change-vm-machine-type subcommand.

--- cmd/vm-control/changeVmMachineType.go
+++ cmd/vm-control/changeVmMachineType.go
@@ -8,29 +8,29 @@ import (
 	"github.com/Cloud-Foundations/Dominator/lib/log"
 )
 
-func changeVmConsoleTypeSubcommand(args []string,
+func changeVmMachineTypeSubcommand(args []string,
 	logger log.DebugLogger) error {
-	if err := changeVmConsoleType(args[0], logger); err != nil {
-		return fmt.Errorf("error changing VM console type: %s", err)
+	if err := changeVmMachineType(args[0], logger); err != nil {
+		return fmt.Errorf("error changing VM machine type: %s", err)
 	}
 	return nil
 }
 
-func changeVmConsoleType(vmHostname string,
+func changeVmMachineType(vmHostname string,
 	logger log.DebugLogger) error {
 	if vmIP, hypervisor, err := lookupVmAndHypervisor(vmHostname); err != nil {
 		return err
 	} else {
-		return changeVmConsoleTypeOnHypervisor(hypervisor, vmIP, logger)
+		return changeVmMachineTypeOnHypervisor(hypervisor, vmIP, logger)
 	}
 }
 
-func changeVmConsoleTypeOnHypervisor(hypervisor string, ipAddr net.IP,
+func changeVmMachineTypeOnHypervisor(hypervisor string, ipAddr net.IP,
 	logger log.DebugLogger) error {
 	client, err := dialHypervisor(hypervisor)
 	if err != nil {
 		return err
 	}
 	defer client.Close()
-	return hyperclient.ChangeVmConsoleType(client, ipAddr, consoleType)
+	return hyperclient.ChangeVmMachineType(client, ipAddr, machineType)
 }
