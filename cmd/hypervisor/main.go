Cloud-Foundations/Dominator

commit 4320dd83f98482e10230c9d57ad4bc3b3de40ede

cmd/hypervisor: report MTU when reporting bridge details.

cmd/hypervisor/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 	"syscall"
 	"time"
 
@@ -157,10 +156,9 @@ func run() {
 		if *dhcpServerOnBridgesOnly {
 			dhcpInterfaces = append(dhcpInterfaces, bridge.Name)
 		}
-		if !strings.HasPrefix(bridge.Name, "br@") {
-			vlanIdToBridge[uint(vlanId)] = bridge.Name
-			logger.Printf("Bridge: %s, VLAN Id: %d\n", bridge.Name, vlanId)
-		}
+		vlanIdToBridge[uint(vlanId)] = bridge.Name
+		logger.Printf("Bridge: %s, VLAN Id: %d, MTU: %d\n",
+			bridge.Name, vlanId, bridge.MTU)
 	}
 	dhcpServer, err := dhcpd.New(dhcpInterfaces,
 		filepath.Join(*stateDir, "dynamic-leases.json"), logger)

commit 3b581a5c34c76281513eb39e4a1e58e0c1fccbea

cmd/hypervisor: fix bug introduced in 4320dd83: bridge map collision.

cmd/hypervisor/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"syscall"
 	"time"
 
@@ -156,9 +157,14 @@ func run() {
 		if *dhcpServerOnBridgesOnly {
 			dhcpInterfaces = append(dhcpInterfaces, bridge.Name)
 		}
-		vlanIdToBridge[uint(vlanId)] = bridge.Name
-		logger.Printf("Bridge: %s, VLAN Id: %d, MTU: %d\n",
-			bridge.Name, vlanId, bridge.MTU)
+		if !strings.HasPrefix(bridge.Name, "br@") {
+			vlanIdToBridge[uint(vlanId)] = bridge.Name
+			logger.Printf("Bridge: %s, VLAN Id: %d (added to map), MTU: %d\n",
+				bridge.Name, vlanId, bridge.MTU)
+		} else {
+			logger.Printf("Bridge: %s, VLAN Id: %d, MTU: %d\n",
+				bridge.Name, vlanId, bridge.MTU)
+		}
 	}
 	dhcpServer, err := dhcpd.New(dhcpInterfaces,
 		filepath.Join(*stateDir, "dynamic-leases.json"), logger)