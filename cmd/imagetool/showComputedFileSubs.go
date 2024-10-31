cd9d35f2c760f55c06d371ceb7d3af810be8d828
Richard Gooch <rg+github0@safe-mbox.com>
2024-10-31 08:45:07 -0700

    cmd/imagetool: add show-computed-file-subs subcommand.

---
+++ cmd/imagetool/showComputedFileSubs.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Cloud-Foundations/Dominator/imageserver/client"
+	"github.com/Cloud-Foundations/Dominator/lib/errors"
+	"github.com/Cloud-Foundations/Dominator/lib/filesystem"
+	"github.com/Cloud-Foundations/Dominator/lib/log"
+	"github.com/Cloud-Foundations/Dominator/lib/mdb"
+	"github.com/Cloud-Foundations/Dominator/lib/srpc"
+	"github.com/Cloud-Foundations/Dominator/lib/verstr"
+	"github.com/Cloud-Foundations/Dominator/proto/mdbserver"
+)
+
+func showComputedFileSubsSubcommand(args []string,
+	logger log.DebugLogger) error {
+	imageSClient, _ := getClients()
+	mdbdSClient, err := dialMdbd()
+	if err != nil {
+		return err
+	}
+	cf := filesystem.ComputedFile{
+		Filename: args[0],
+		Source:   args[1],
+	}
+	if err := showComputedFileSubs(imageSClient, mdbdSClient, cf); err != nil {
+		return fmt.Errorf("error listing images not in MDB: %s", err)
+	}
+	return nil
+}
+
+func showComputedFileSubs(imageSClient, mdbdSClient srpc.ClientI,
+	computedFile filesystem.ComputedFile) error {
+	// Get data from MDB.
+	request := mdbserver.GetMdbRequest{}
+	var reply mdbserver.GetMdbResponse
+	err := mdbdSClient.RequestReply("MdbServer.GetMdb", request, &reply)
+	if err != nil {
+		return err
+	}
+	if err := errors.New(reply.Error); err != nil {
+		return err
+	}
+	// Compute mapping from image to machine.
+	imageToMachinesMap := make(map[string][]*mdb.Machine)
+	for index := range reply.Machines {
+		machine := &reply.Machines[index]
+		if machine.RequiredImage == "" {
+			continue
+		}
+		imageToMachinesMap[machine.RequiredImage] = append(
+			imageToMachinesMap[machine.RequiredImage], machine)
+	}
+	machinesMap := make(map[*mdb.Machine]struct{})
+	for imageName, imageMachines := range imageToMachinesMap {
+		computedFiles, _, err := client.GetImageComputedFiles(imageSClient,
+			imageName)
+		if err != nil {
+			return err
+		}
+		for _, imageComputedFile := range computedFiles {
+			if imageComputedFile == computedFile {
+				for _, machine := range imageMachines {
+					machinesMap[machine] = struct{}{}
+				}
+				break
+			}
+		}
+	}
+	hostnames := make([]string, 0, len(machinesMap))
+	hostnameToImageMap := make(map[string]string, len(machinesMap))
+	for machine := range machinesMap {
+		hostnames = append(hostnames, machine.Hostname)
+		hostnameToImageMap[machine.Hostname] = machine.RequiredImage
+	}
+	verstr.Sort(hostnames)
+	for _, hostname := range hostnames {
+		fmt.Printf("%-30s %s\n", hostname, hostnameToImageMap[hostname])
+	}
+	return nil
+}
