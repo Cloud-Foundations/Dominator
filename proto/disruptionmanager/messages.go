8cd331616776b8e3cf854c95160ad3a11fd4e9ca
Richard Gooch <rg+github0@safe-mbox.com>
2024-11-13 08:15:48 -0800

    proto/disruptionmanager: add Disruption{Cancel,Check,Request} messages.

--- proto/disruptionmanager/messages.go
+++ proto/disruptionmanager/messages.go
@@ -5,12 +5,47 @@ import (
 	"github.com/Cloud-Foundations/Dominator/proto/sub"
 )
 
+// DisruptionCancel RPC request.
+type DisruptionCancelRequest struct {
+	MDB mdb.Machine
+}
+
+// DisruptionCancel RPC response.
+type DisruptionCancelResponse struct {
+	Error    string
+	Response sub.DisruptionState
+}
+
+// DisruptionCheck RPC request.
+type DisruptionCheckRequest struct {
+	MDB mdb.Machine
+}
+
+// DisruptionCheck RPC response.
+type DisruptionCheckResponse struct {
+	Error    string
+	Response sub.DisruptionState
+}
+
+// REST endpoint request.
 type DisruptionRequest struct {
 	MDB     mdb.Machine
 	Request sub.DisruptionRequest
 }
 
+// REST endpoint response.
 type DisruptionResponse struct {
+	Response sub.DisruptionState
+}
+
+// DisruptionCheck RPC request.
+type DisruptionRequestRequest struct {
+	MDB mdb.Machine
+}
+
+// DisruptionRequest RPC response.
+type DisruptionRequestResponse struct {
+	Error    string
 	Response sub.DisruptionState
 }
 
