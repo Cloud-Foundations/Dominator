d3f4f7bfa78a855a4ad415a9242910acfe5fcae3
Richard Gooch <rg+github0@safe-mbox.com>
2024-12-10 07:21:47 -0800

    cmd/show-cert: add support for Ed25519 and SSH certs from Metadata service.

--- cmd/show-cert/main.go
+++ cmd/show-cert/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -12,9 +13,38 @@ import (
 
 	"github.com/Cloud-Foundations/Dominator/lib/constants"
 	"github.com/Cloud-Foundations/Dominator/lib/format"
+	"github.com/Cloud-Foundations/Dominator/lib/sshutil"
 	"github.com/Cloud-Foundations/Dominator/lib/stringutil"
 	"github.com/Cloud-Foundations/Dominator/lib/x509util"
 )
+
+type metadataCertificateType struct {
+	certType string
+	path     string
+	isSsh    bool
+}
+
+var (
+	metadataCertificates = []metadataCertificateType{
+		{"RSA/X.509", constants.MetadataIdentityCert, false},
+		{"RSA/SSH", constants.MetadataIdentityRsaSshCert, true},
+		{"Ed25519/X.509", constants.MetadataIdentityEd25519X509Cert, false},
+		{"Ed25519/SSH", constants.MetadataIdentityEd25519SshCert, true},
+	}
+)
+
+func getFromMetadata(path string) ([]byte, error) {
+	client := http.Client{Timeout: 100 * time.Millisecond}
+	resp, err := client.Get(constants.MetadataUrl + path)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	return io.ReadAll(resp.Body)
+}
 
 func showCertFile(filename string) {
 	data, err := ioutil.ReadFile(filename)
@@ -23,29 +53,54 @@ func showCertFile(filename string) {
 		return
 	}
 	fmt.Println("Certificate:", filename+":")
-	showCert(data)
+	showX509Cert(data)
 }
 
 func showCertMetadata() {
-	client := http.Client{Timeout: 100 * time.Millisecond}
-	resp, err := client.Get(constants.MetadataUrl +
-		constants.MetadataIdentityCert)
-	if err != nil {
-		return
+	for _, mCert := range metadataCertificates {
+		fmt.Println("Certificate:", "MetadataIdentity:", mCert.certType+":")
+		data, err := getFromMetadata(mCert.path)
+		if err != nil {
+			return
+		}
+		if mCert.isSsh {
+			showSshCert(data)
+		} else {
+			showX509Cert(data)
+		}
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return
-	}
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return
-	}
-	fmt.Println("Certificate:", "MetadataIdentity:")
-	showCert(data)
 }
 
-func showCert(data []byte) {
+func showList(list map[string]struct{}) {
+	sortedList := stringutil.ConvertMapKeysToList(list, true)
+	for _, entry := range sortedList {
+		fmt.Println("   ", entry)
+	}
+}
+
+func showSshCert(data []byte) {
+	_, cert, _, err := sshutil.ParseCertificate(data)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	notAfter := time.Unix(int64(cert.ValidBefore), 0)
+	notBefore := time.Unix(int64(cert.ValidAfter), 0)
+	now := time.Now()
+	if notYet := notBefore.Sub(now); notYet > 0 {
+		fmt.Fprintf(os.Stderr, "  Will not be valid for %s\n",
+			format.Duration(notYet))
+	}
+	if expired := now.Sub(notAfter); expired > 0 {
+		fmt.Fprintf(os.Stderr, "  Expired %s ago\n", format.Duration(expired))
+	} else {
+		fmt.Fprintf(os.Stderr, "  Valid until %s (%s from now)\n",
+			notAfter, format.Duration(-expired))
+	}
+	fmt.Printf("  Principals: %v\n", cert.ValidPrincipals)
+}
+
+func showX509Cert(data []byte) {
 	block, rest := pem.Decode(data)
 	if block == nil {
 		fmt.Fprintf(os.Stderr, "Failed to parse certificate PEM")
@@ -98,13 +153,6 @@ func showCert(data []byte) {
 		showList(groupList)
 	} else {
 		fmt.Println("  No group memberships")
-	}
-}
-
-func showList(list map[string]struct{}) {
-	sortedList := stringutil.ConvertMapKeysToList(list, true)
-	for _, entry := range sortedList {
-		fmt.Println("   ", entry)
 	}
 }
 
