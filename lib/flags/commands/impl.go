3d2604471659f3b5386b4f9c3afe29c57558cd65
Richard Gooch <rg+github0@safe-mbox.com>
2025-02-19 06:03:43 -0800

    lib/flags/commands: print warning for unsorted commands and skip nil funcs.

--- lib/flags/commands/impl.go
+++ lib/flags/commands/impl.go
@@ -4,17 +4,30 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"sort"
 
 	"github.com/Cloud-Foundations/Dominator/lib/log"
 )
 
 func printCommands(writer io.Writer, commands []Command) {
+	isSorted := sort.SliceIsSorted(commands, func(i, j int) bool {
+		return commands[i].Command < commands[j].Command
+	})
+	if !isSorted {
+		fmt.Fprintln(writer, "NOTE: COMMANDS ARE NOT SORTED!")
+	}
 	for _, command := range commands {
+		if command.CmdFunc == nil {
+			continue
+		}
 		if command.Args == "" {
 			fmt.Fprintln(writer, " ", command.Command)
 		} else {
 			fmt.Fprintln(writer, " ", command.Command, command.Args)
 		}
+	}
+	if !isSorted {
+		fmt.Fprintln(writer, "NOTE: COMMANDS ARE NOT SORTED!")
 	}
 }
 
@@ -22,6 +35,9 @@ func runCommands(commands []Command, printUsage func(),
 	logger log.DebugLogger) int {
 	numCommandArgs := flag.NArg() - 1
 	for _, command := range commands {
+		if command.CmdFunc == nil {
+			continue
+		}
 		if flag.Arg(0) == command.Command {
 			if numCommandArgs < command.MinArgs ||
 				(command.MaxArgs >= 0 &&
