e2caf300f94de2db498248d524cf3797b7b82b58
Richard Gooch <rg+github0@safe-mbox.com>
2025-04-04 08:31:48 -0700

    imageserver/rpcd: log GetImageArchive SRPC method.

--- imageserver/rpcd/getImageArchive.go
+++ imageserver/rpcd/getImageArchive.go
@@ -11,6 +11,12 @@ func (t *srpcType) GetImageArchive(conn *srpc.Conn,
 	reply *imageserver.GetImageArchiveResponse) error {
 	var response imageserver.GetImageArchiveResponse
 	if t.replicationMaster == "" {
+		if username := conn.Username(); username == "" {
+			t.logger.Printf("GetImageArchive(%s)\n", request.ImageName)
+		} else {
+			t.logger.Printf("GetImageArchive(%s) by %s\n",
+				request.ImageName, username)
+		}
 		archiveData, err := t.imageDataBase.GetImageArchive(request.ImageName)
 		response.ArchiveData = archiveData
 		response.Error = errors.ErrorToString(err)
