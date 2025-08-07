940adf305a25623da8e6fd35a1ce6bcd17e004f0
Richard Gooch <rg+github0@safe-mbox.com>
2025-08-07 08:38:21 -0700

    lib/gitutil: ShallowClone(): try sparse checkout (needs newer Git version).

--- lib/gitutil/shallowClone.go
+++ lib/gitutil/shallowClone.go
@@ -2,6 +2,8 @@ package gitutil
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -15,6 +17,10 @@ import (
 	"github.com/Cloud-Foundations/Dominator/lib/fsutil"
 	"github.com/Cloud-Foundations/Dominator/lib/log"
 	"github.com/Cloud-Foundations/Dominator/lib/stringutil"
+)
+
+var (
+	errorGitTooOld = errors.New("Git too old")
 )
 
 func gitGrab(topdir string, gitBranch string, logger log.DebugLogger) error {
@@ -88,6 +94,24 @@ func shallowClone(topdir string, params ShallowCloneParams,
 		logger.Debugf(0, "Cloning repository: %s\n", params.PublicURL)
 	}
 	startTime := time.Now()
+	if err := shallowCloneSelect(topdir, params, logger); err != nil {
+		return err
+	}
+	loadTime := time.Since(startTime)
+	repoSize, err := fsutil.GetTreeSize(topdir)
+	if err != nil {
+		return err
+	}
+	speed := float64(repoSize) / loadTime.Seconds()
+	logger.Debugf(0,
+		"Downloaded partial repository in %s, size: %s (%s/s)\n",
+		format.Duration(loadTime), format.FormatBytes(repoSize),
+		format.FormatBytes(uint64(speed)))
+	return nil
+}
+
+func shallowCloneOriginal(topdir string, params ShallowCloneParams,
+	logger log.DebugLogger) error {
 	err := runCommand(logger, "", "git", "init", "-b", "master", topdir)
 	if err != nil {
 		return err
@@ -121,15 +145,68 @@ func shallowClone(topdir string, params ShallowCloneParams,
 	if err := gitGrab(topdir, params.GitBranch, logger); err != nil {
 		return err
 	}
-	loadTime := time.Since(startTime)
-	repoSize, err := fsutil.GetTreeSize(topdir)
+	return nil
+}
+
+func shallowCloneSelect(topdir string, params ShallowCloneParams,
+	logger log.DebugLogger) error {
+	if params.GitBranch == "" {
+		return shallowCloneOriginal(topdir, params, logger)
+	}
+	if len(params.Patterns) == 1 { // HACK
+		pattern := params.Patterns[0]
+		if pattern == "**/manifest" {
+			return shallowCloneOriginal(topdir, params, logger)
+		}
+		if !strings.HasSuffix(pattern, "/*") {
+			return shallowCloneOriginal(topdir, params, logger)
+		}
+		params.Patterns[0] = pattern[:len(pattern)-2]
+	}
+	if err := tryBetterShallowClone(topdir, params, logger); err == nil {
+		return nil
+	} else if err == errorGitTooOld {
+		return shallowCloneOriginal(topdir, params, logger)
+	} else {
+		return err
+	}
+}
+
+// tryBetterShallowClone tries a more efficient shallow clone using a newer
+// version of Git.
+func tryBetterShallowClone(topdir string, params ShallowCloneParams,
+	logger log.DebugLogger) error {
+	cmd := exec.Command("git", "clone", "--filter=blob:none",
+		"--no-checkout", "--depth", "1", "--revision", params.GitBranch,
+		"--sparse", params.RepoURL, topdir)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		if bytes.Contains(output, []byte("unknown option")) {
+			return errorGitTooOld
+		}
+		output = bytes.TrimSpace(output)
+		if writer, ok := logger.(io.Writer); ok {
+			writer.Write(output)
+			return fmt.Errorf("error running: git clone: %s", err)
+		}
+		return fmt.Errorf("error running: git clone: %s: %s",
+			err, string(output))
+	}
+	err = runCommand(logger, topdir, "git", "config", "core.sparseCheckout",
+		"true")
 	if err != nil {
 		return err
 	}
-	speed := float64(repoSize) / loadTime.Seconds()
-	logger.Debugf(0,
-		"Downloaded partial repository in %s, size: %s (%s/s)\n",
-		format.Duration(loadTime), format.FormatBytes(repoSize),
-		format.FormatBytes(uint64(speed)))
-	return nil
+	err = runCommand(logger, topdir, "git", "config", "core.sparseCheckoutCone",
+		"true")
+	if err != nil {
+		return err
+	}
+	args := []string{"git", "sparse-checkout", "set"}
+	args = append(args, params.Patterns...)
+	err = runCommand(logger, topdir, args...)
+	if err != nil {
+		return err
+	}
+	return runCommand(logger, topdir, "git", "checkout")
 }
