9d7d4c0e75e7736c8b3d204b17d2ae6fee76966a
Richard Gooch <rg+github0@safe-mbox.com>
2025-09-21 19:21:51 -0700

    cmd/hyper-control: {get-updates,watch-dhcp}: use new getter/watcher helpers

--- cmd/hyper-control/watchDhcp.go
+++ cmd/hyper-control/watchDhcp.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 
+	hyperclient "github.com/Cloud-Foundations/Dominator/hypervisor/client"
 	"github.com/Cloud-Foundations/Dominator/lib/errors"
 	"github.com/Cloud-Foundations/Dominator/lib/fsutil"
 	"github.com/Cloud-Foundations/Dominator/lib/log"
@@ -34,57 +35,41 @@ func watchDhcp(interfaceName string, logger log.DebugLogger) error {
 		return err
 	}
 	defer client.Close()
-	conn, err := client.Call("Hypervisor.WatchDhcp")
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
 	request := proto.WatchDhcpRequest{Interface: interfaceName}
-	if err := conn.Encode(request); err != nil {
-		return err
-	}
-	if err := conn.Flush(); err != nil {
-		return err
-	}
 	dirname, err := os.MkdirTemp("",
 		"hyper-control.watch-dhcp."+*hypervisorHostname+".")
 	if err != nil {
 		return err
 	}
 	logger.Printf("Results in directory: %s\n", dirname)
-	for counter := 0; true; counter++ {
-		var reply proto.WatchDhcpResponse
-		if err := conn.Decode(&reply); err != nil {
-			return err
-		}
-		if err := errors.New(reply.Error); err != nil {
-			return err
-		}
-		filename := fmt.Sprintf("%s.%.5d", reply.Interface, counter)
-		file, err := os.Create(filepath.Join(dirname, filename))
-		if err != nil {
-			return err
-		}
-		file.Write(reply.Packet)
-		file.Close()
-		packet := dhcp.Packet(reply.Packet)
-		options := packet.ParseOptions()
-		msgType := dhcp.MessageType(options[dhcp.OptionDHCPMessageType][0])
-		logger.Printf("Counter: %d, message type: %s, from: %s, options:\n",
-			counter, msgType, packet.CHAddr())
-		optionsDirname := filepath.Join(dirname, filename) + ".options"
-		os.Mkdir(optionsDirname, fsutil.DirPerms)
-		for code, value := range options {
-			logger.Printf("  Code: %s, value: %0x\n", code, value)
-			optionFilename := fmt.Sprintf("%s/%d_%s",
-				optionsDirname, code, code)
-			file, err := os.Create(optionFilename)
+	var counter uint
+	return hyperclient.WatchDhcp(client, request,
+		func(ifName string, rawPacket []byte) error {
+			filename := fmt.Sprintf("%s.%.5d", ifName, counter)
+			file, err := os.Create(filepath.Join(dirname, filename))
 			if err != nil {
 				return err
 			}
-			file.Write(value)
+			file.Write(rawPacket)
 			file.Close()
-		}
-	}
-	return nil
+			packet := dhcp.Packet(rawPacket)
+			options := packet.ParseOptions()
+			msgType := dhcp.MessageType(options[dhcp.OptionDHCPMessageType][0])
+			logger.Printf("Counter: %d, message type: %s, from: %s, options:\n",
+				counter, msgType, packet.CHAddr())
+			optionsDirname := filepath.Join(dirname, filename) + ".options"
+			os.Mkdir(optionsDirname, fsutil.DirPerms)
+			for code, value := range options {
+				logger.Printf("  Code: %s, value: %0x\n", code, value)
+				optionFilename := fmt.Sprintf("%s/%d_%s",
+					optionsDirname, code, code)
+				file, err := os.Create(optionFilename)
+				if err != nil {
+					return err
+				}
+				file.Write(value)
+				file.Close()
+			}
+			return nil
+		})
 }
