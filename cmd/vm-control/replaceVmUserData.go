24b0af8d6f88c2a04b1a00ae91e1dcceddafe39e
Richard Gooch <rg+github0@safe-mbox.com>
2025-09-23 09:33:20 -0700

    cmd/vm-control: replace-vm-user-data: make use of new helper function.

--- cmd/vm-control/replaceVmUserData.go
+++ cmd/vm-control/replaceVmUserData.go
@@ -3,12 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"net"
 
+	hyperclient "github.com/Cloud-Foundations/Dominator/hypervisor/client"
 	"github.com/Cloud-Foundations/Dominator/lib/errors"
 	"github.com/Cloud-Foundations/Dominator/lib/log"
-	proto "github.com/Cloud-Foundations/Dominator/proto/hypervisor"
 )
 
 func replaceVmUserDataSubcommand(args []string, logger log.DebugLogger) error {
@@ -36,41 +35,11 @@ func replaceVmUserDataOnHypervisor(hypervisor string, ipAddr net.IP,
 		return err
 	}
 	defer file.Close()
-	request := proto.ReplaceVmUserDataRequest{
-		IpAddress: ipAddr,
-		Size:      uint64(size),
-	}
-	userDataReader := bufio.NewReader(io.LimitReader(file, size))
 	client, err := dialHypervisor(hypervisor)
 	if err != nil {
 		return err
 	}
 	defer client.Close()
-	conn, err := client.Call("Hypervisor.ReplaceVmUserData")
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	if err := conn.Encode(request); err != nil {
-		return err
-	}
-	logger.Debugln(0, "uploading user data")
-	if _, err := io.Copy(conn, userDataReader); err != nil {
-		return err
-	}
-	if err := conn.Flush(); err != nil {
-		return err
-	}
-	errorString, err := conn.ReadString('\n')
-	if err != nil {
-		return err
-	}
-	if err := errors.New(errorString[:len(errorString)-1]); err != nil {
-		return err
-	}
-	var response proto.ReplaceVmUserDataResponse
-	if err := conn.Decode(&response); err != nil {
-		return err
-	}
-	return errors.New(response.Error)
+	return hyperclient.ReplaceVmUserData(client, ipAddr, bufio.NewReader(file),
+		uint64(size), logger)
 }
