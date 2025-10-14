5c8b7ac0d8d0e6ef21a54902c718ddc23da1709f
Richard Gooch <rg+github0@safe-mbox.com>
2025-10-14 09:41:04 -0700

    lib/srpc/setupserver: add Params.PermitInsecure field.

--- lib/srpc/setupserver/api.go
+++ lib/srpc/setupserver/api.go
@@ -1,15 +1,16 @@
 /*
-	Package setupserver assists in setting up TLS credentials for a server.
+Package setupserver assists in setting up TLS credentials for a server.
 
-	Package setupserver provides convenience functions for setting up a server
-	with TLS credentials.
+Package setupserver provides convenience functions for setting up a server
+with TLS credentials.
 
-	The package loads client and server certificates from files and registers
-	them with the lib/srpc package. The following command-line flags are
-	registered with the standard flag package:
-	  -caFile:   Name of file containing the root of trust
-	  -certFile: Name of file containing the SSL certificate
-	  -keyFile:  Name of file containing the SSL key
+The package loads client and server certificates from files and registers
+them with the lib/srpc package. The following command-line flags are
+registered with the standard flag package:
+
+	-caFile:   Name of file containing the root of trust
+	-certFile: Name of file containing the SSL certificate
+	-keyFile:  Name of file containing the SSL key
 */
 package setupserver
 
@@ -18,9 +19,10 @@ import (
 )
 
 type Params struct {
-	ClientOnly    bool // If true, only register client certificate and key.
-	FailIfExpired bool // If true, fail if certificate not yet valid or expired.
-	Logger        log.DebugLogger
+	ClientOnly     bool // If true, only register client certificate and key.
+	FailIfExpired  bool // If true, fail if certificate not yet valid or expired
+	Logger         log.DebugLogger
+	PermitInsecure bool // If true, ignore missing CA.
 }
 
 func SetupTls() error {
