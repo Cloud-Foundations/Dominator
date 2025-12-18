1277a45988904ea3207da3cd6aa78ebd47a1135b
Richard Gooch <rg+github0@safe-mbox.com>
2025-12-18 07:58:21 -0800

    proto/dominator: add FastUpdateRequest.QueueTimeout field.

--- proto/dominator/messages.go
+++ proto/dominator/messages.go
@@ -35,15 +35,18 @@ type FastUpdateRequest struct {
 	FailOnReboot          bool
 	ForceDisruptiveUpdate bool
 	Hostname              string
+	QueueTimeout          time.Duration // Default: infinite.
 	Timeout               time.Duration // Default: 15 minutes.
 	UsePlannedImage       bool
 }
 
 type FastUpdateResponse struct { // Multiple responses are sent.
-	Error           string // If non-empty, this is the final response.
-	Final           bool   // If true, this is the final response.
-	ProgressMessage string // Status updates and errors.
-	Synced          bool   // If true, the sub is synced with the image.
+	Error           string        // If non-empty, this is the final response.
+	Final           bool          // If true, this is the final response.
+	ProcessingTime  time.Duration // Time spent processing the update,<0:timeout
+	ProgressMessage string        // Status updates and errors.
+	QueueTime       time.Duration // Time spent waiting in the queue, <0:timeout
+	Synced          bool          // If true, the sub is synced with the image.
 }
 
 type ForceDisruptiveUpdateRequest struct {
