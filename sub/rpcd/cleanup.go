8bf9eff7895eb4467e1c26eb12c75ebf851bb59d
Richard Gooch <rg+github0@safe-mbox.com>
2026-01-05 07:35:33 -0800

    sub/rpcd: Update: avoid boosting CPU limit when no objects removed.

--- sub/rpcd/cleanup.go
+++ sub/rpcd/cleanup.go
@@ -12,12 +12,12 @@ import (
 
 func (t *rpcType) Cleanup(conn *srpc.Conn, request sub.CleanupRequest,
 	reply *sub.CleanupResponse) error {
-	defer t.params.ScannerConfiguration.BoostCpuLimit(t.params.Logger)
 	t.params.DisableScannerFunction(true)
 	defer t.params.DisableScannerFunction(false)
 	t.rwLock.Lock()
 	defer t.rwLock.Unlock()
-	t.params.Logger.Printf("Cleanup(): %d objects\n", len(request.Hashes))
+	t.params.Logger.Printf("Cleanup(%s): %d objects\n",
+		conn.Username(), len(request.Hashes))
 	if t.fetchInProgress {
 		t.params.Logger.Println("Error: fetch in progress")
 		return errors.New("fetch in progress")
@@ -26,6 +26,7 @@ func (t *rpcType) Cleanup(conn *srpc.Conn, request sub.CleanupRequest,
 		t.params.Logger.Println("Error: update progress")
 		return errors.New("update in progress")
 	}
+	var changed bool
 	for _, hash := range request.Hashes {
 		pathname := path.Join(t.config.ObjectsDirectoryName,
 			objectcache.HashToFilename(hash))
@@ -34,11 +35,15 @@ func (t *rpcType) Cleanup(conn *srpc.Conn, request sub.CleanupRequest,
 			err = fsutil.ForceRemove(pathname)
 		})
 		if err == nil {
+			changed = true
 			t.params.Logger.Printf("Deleted: %s\n", pathname)
 		} else {
 			t.params.Logger.Println(err)
 		}
 	}
 	t.disruptionCancel()
+	if changed {
+		t.params.ScannerConfiguration.BoostCpuLimit(t.params.Logger)
+	}
 	return nil
 }
