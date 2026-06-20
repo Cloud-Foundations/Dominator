Cloud-Foundations/Dominator

commit 855c1a27410f97153bf37bf4fbb48ebee3009043

lib/fsutil: CopyToFileExclusive(): include filename in exist error.

lib/fsutil/copy.go
@@ -33,7 +33,7 @@ func copyToFileExclusive(destFilename string, perm os.FileMode,
 	reader io.Reader, length uint64) error {
 	// First do a read-only test for existence, to limit file-system mutations.
 	if _, err := os.Stat(destFilename); err == nil {
-		return os.ErrExist
+		return fmt.Errorf("open %s: file exists", destFilename)
 	}
 	tmpFilename := destFilename + "~"
 	destFile, err := os.OpenFile(tmpFilename, os.O_CREATE|os.O_EXCL|os.O_WRONLY,
@@ -46,7 +46,7 @@ func copyToFileExclusive(destFilename string, perm os.FileMode,
 	// At this point we own the tmpfile and implicitly the destfile. Do a quick
 	// check so that we don't waste time writing if it's going to fail later.
 	if _, err := os.Stat(destFilename); err == nil {
-		return os.ErrExist
+		return fmt.Errorf("open %s: file exists", destFilename)
 	}
 	if err := copyToWriter(destFile, tmpFilename, reader, length); err != nil {
 		return err

commit 8a52e23cfb50b1b84af52228bc55bb4d3df4e2ad

lib/fsutil: restore os.PathError which regressed in commit 855c1a27.

lib/fsutil/copy.go
@@ -33,7 +33,11 @@ func copyToFileExclusive(destFilename string, perm os.FileMode,
 	reader io.Reader, length uint64) error {
 	// First do a read-only test for existence, to limit file-system mutations.
 	if _, err := os.Stat(destFilename); err == nil {
-		return fmt.Errorf("open %s: file exists", destFilename)
+		return &os.PathError{
+			Op:   "open",
+			Path: destFilename,
+			Err:  os.ErrExist,
+		}
 	}
 	tmpFilename := destFilename + "~"
 	destFile, err := os.OpenFile(tmpFilename, os.O_CREATE|os.O_EXCL|os.O_WRONLY,
@@ -46,7 +50,11 @@ func copyToFileExclusive(destFilename string, perm os.FileMode,
 	// At this point we own the tmpfile and implicitly the destfile. Do a quick
 	// check so that we don't waste time writing if it's going to fail later.
 	if _, err := os.Stat(destFilename); err == nil {
-		return fmt.Errorf("open %s: file exists", destFilename)
+		return &os.PathError{
+			Op:   "open",
+			Path: destFilename,
+			Err:  os.ErrExist,
+		}
 	}
 	if err := copyToWriter(destFile, tmpFilename, reader, length); err != nil {
 		return err