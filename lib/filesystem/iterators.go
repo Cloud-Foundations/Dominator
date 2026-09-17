259117d48645d88f370e2d69fa430c34f66d88f1
Richard Gooch <rg+github0@safe-mbox.com>
2026-09-17 07:19:58 -0700

    lib/filesystem: add FileSystem.ForEachDirectory() method and warning.

--- lib/filesystem/iterators.go
+++ lib/filesystem/iterators.go
@@ -4,6 +4,25 @@ import (
 	"path"
 )
 
+func (directory *DirectoryInode) forEachDirectory(directoryName string,
+	fn func(name string, inodeNumber uint64,
+		inode *DirectoryInode) error) error {
+	for _, dirent := range directory.EntryList {
+		inode, ok := dirent.inode.(*DirectoryInode)
+		if !ok {
+			continue
+		}
+		name := path.Join(directoryName, dirent.Name)
+		if err := fn(name, dirent.InodeNumber, inode); err != nil {
+			return err
+		}
+		if err := inode.forEachDirectory(name, fn); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (directory *DirectoryInode) forEachEntry(directoryName string,
 	fn func(name string, inodeNumber uint64, inode GenericInode) error) error {
 	for _, dirent := range directory.EntryList {
@@ -18,6 +37,15 @@ func (directory *DirectoryInode) forEachEntry(directoryName string,
 		}
 	}
 	return nil
+}
+
+func (fs *FileSystem) forEachDirectory(
+	fn func(name string, inodeNumber uint64,
+		inode *DirectoryInode) error) error {
+	if err := fn("/", 0, &fs.DirectoryInode); err != nil {
+		return err
+	}
+	return fs.DirectoryInode.forEachDirectory("/", fn)
 }
 
 func (fs *FileSystem) forEachFile(
